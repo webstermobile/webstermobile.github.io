677a3d036e377400285b8c1ed97c7f346d072f8f
zhangpeili <zhangpeili@windward.com.cn>
2017-03-20 10:01:22 +0800

    del zhuiyu

--- js/setfont.js
+++
@@ -1,7 +0,0 @@
-function setfont(){
-var deviceWidth = document.documentElement.clientWidth;
-if(deviceWidth >= 750) deviceWidth = 750;
-document.documentElement.style.fontSize = deviceWidth / 7.5 + 'px';
-}
-setfont()
-window.onresize=function(){setfont()}
